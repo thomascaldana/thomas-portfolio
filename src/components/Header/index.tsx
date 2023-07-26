c81083c91fd97d9554d2d6433be86cb3b9df100a
thomascaldana <thomascaldana@gmail.com>
2023-07-26 11:23:25 -0300

    first part of converting css to styledcomp done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Nav } from '../Header/styles'
+import { Container, Nav, MenuOptions } from '../Header/styles'
 import { SiLinkedin } from "react-icons/si"
 // import { GiHamburgerMenu } from "react-icons/gi"
 import { useState } from 'react'
@@ -6,29 +6,6 @@ import './navbar.css'
 import { Name } from '../Header/styles'
 
 const Header = (): JSX.Element => {
-
-  /*
-    return (
-      <>
-      <Container>
-        <Name>
-           <p>Thomás Caldana</p>
-          <SiLinkedin />
-        </Name>
-        <Menu>
-          <Button><GiHamburgerMenu className='menu-icon' /></Button>
-        </Menu>
-      </Container>
-      <div className='burguer-menu'>
-        <ul>
-          <li className={burguerClass}><a>Home</a></li>
-          <li className={burguerClass}><a>Projects</a></li>
-          <li className={burguerClass}><a>Contact</a></li>
-        </ul>
-      </div>
-      </>
-    ) */
-
 
   // to change burger classes
   const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
@@ -51,7 +28,7 @@ const Header = (): JSX.Element => {
   return (
     <Container style={{ width: '100%' }}>
       <Nav>
-      <Name>
+        <Name>
            <p>Thomás Caldana</p>
           <SiLinkedin />
         </Name>
@@ -62,14 +39,14 @@ const Header = (): JSX.Element => {
         </div>
       </Nav>
 
-      <div className={menu_class}>
+      <MenuOptions className={menu_class}>
         <ul className='menu-options'>
           <li className='li-item'><a>Home</a></li>
           <li className='li-item'><a>Projects</a></li>
           <li className='li-item'><a>Contact</a></li>
           <li className='li-item'><a>Social Media</a></li>
         </ul>
-      </div>
+      </MenuOptions>
     </Container>
   )
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -42,9 +42,63 @@ export const Menu = styled.div`
 }
 ` */
 
-export const Container = styled.div`
-  background: #F1F1F1
+export const Container = styled.header`
+  background: #F1F1F1;
+
+  .menu {
+  width: 100%;
+  height: 12rem;
+  background-color: rgb(206, 220, 250);
+  top: 0;
+  z-index: -1;
+}
+
+.menu-options {
+  padding: 20px 5px ;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  gap: 20px;
+  font-size: 1.1rem;
+  font-family: sans-serif;  
+
+  .li-item{
+  list-style-type: none;
+}
+
+
+}
 `
+export const Nav = styled.nav`
+  width: 100%;
+  height: 4em;
+  display: flex;
+  justify-content:space-between;
+  padding: 1em;
+
+  .burger-menu {
+  height: 100%;
+  width: 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  cursor: pointer;
+
+  .burger-bar {
+  width: 2em;
+  height: 0.25em;
+  background-color: #000000;
+  border-radius: 0.5em;
+  
+}
+
+
+}
+  `
+
 export const Name = styled.a`
   display: flex;
   flex-flow: row nowrap;
@@ -57,10 +111,13 @@ export const Name = styled.a`
     font-weight: 700;
   }
   `
-export const Nav = styled.nav`
-  width: 100%;
-  height: 4em;
-  display: flex;
-  justify-content:space-between;
-  padding: 1em;
+
+export const MenuOptions = styled.div`
+.hidden {
+  display: none;
+}
+
+.visible {
+  display: block;
+}
   `
